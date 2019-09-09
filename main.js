90ea1397418029638227c4726f07a02bf19ad2f6
Raheel Anwar <raheelanwar74@gmail.com>
2019-09-09 11:21:36 +0500

    updated

---
+++ main.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+const DB_HOST = '172.16.190.31';
+const DB_NAME = 'AdventureWorks2012';
+const DB_USERNAME = 'admin';
+const DB_PASSWORD = 'Onstak123';
+
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'mssql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Database connection established...');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
